107184a5d8499217b88f0f1ce5d3d4e4ee1549f7
otiai10 <otiai10@gmail.com>
2016-11-04 10:57:49 +0100

    Set listening port dynamically

--- main.go
+++ main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	m "github.com/otiai10/marmoset"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
 	router := m.NewRouter()
 	router.GET("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "Hello!")
 	})
 
-	http.ListenAndServe(":5000", router)
+	http.ListenAndServe(":"+port, router)
 }
