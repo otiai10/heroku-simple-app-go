9fd5a5e99fe4c45b0a83bef78ffebe98f89efe78
otiai10 <otiai10@gmail.com>
2016-11-18 15:56:51 +0100

    Fix output

--- main.go
+++ main.go
@@ -15,7 +15,9 @@ func main() {
 	router.GET("/", func(w http.ResponseWriter, req *http.Request) {
 		m.RenderJSON(w, http.StatusOK, m.P{
 			"TESSDATA_PREFIX": os.Getenv("TESSDATA_PREFIX"),
-			"LD_INCLUDE_PATH": os.Getenv("LD_INCLUDE_PATH"),
+			"INCLUDE_PATH":    os.Getenv("INCLUDE_PATH"),
+			"LIBRARY_PATH":    os.Getenv("LIBRARY_PATH"),
+			"LD_LIBRARY_PATH": os.Getenv("LD_LIBRARY_PATH"),
 		})
 	})
 
